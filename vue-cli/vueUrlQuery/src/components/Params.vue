46194e2e33be19f4a259789201cbd62b1aae877e
tinalim <lst_tina@163.com>
2018-10-30 13:56:37 +0800

    新增路由传参query方式

---
+++ vue-cli/vueUrlQuery/src/components/Params.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <p>newsid: {{$route.params.newsId}}</p>
+    <P>newsTitle: {{$route.params.newsTitle}}</P>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Params',
+    data () {
+      return {
+        msg: 'params page'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
