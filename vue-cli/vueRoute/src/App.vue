a9598d31a29016331a30e4ed9a042d88d0a60e20
tinalim <lst_tina@163.com>
2018-10-30 10:56:37 +0800

    单页多个router-view的操作

---
+++ vue-cli/vueRoute/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/tinalst">change</router-link>
+    <router-view/>
+    <router-view name="left"/>
+    <router-view name="right"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vue-cli/vueRoute/src/components/Hi1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hi1">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default  {
+    data() {
+      return {
+        msg: 'i am hi1'
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .hi1{
+    background: antiquewhite;
+    display: inline-block;
+    width: 49%;
+  }
+</style>
+
