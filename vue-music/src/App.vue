8245af67fc5b6b754f7f81e37d04de436565d304
tinalim <lst_tina@163.com>
2018-11-08 17:16:49 +0800

    2.3

---
+++ vue-music/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+        <div id="app">
+                hello world
+                <router-view/>
+        </div>
+</template>
+
+<script>
+        // export default {
+        //         name: 'App'
+        // }
+</script>
+
+<style lang="scss" >
+        @import "common/scss/index";
+        @import "common/scss/variable";
+        #app {
+                color: $color-theme;
+        }
+</style>
