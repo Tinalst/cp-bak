689eecc1ad692eb20471a034d60bd34a2d6490b4
tinalim <lst_tina@163.com>
2019-01-17 18:59:51 +0800

    update

--- ngStudy/scrollMagicBasic/src/app/simple-tween-scroll/simple-tween-scroll.component.ts
+++ ngStudy/scrollMagicBasic/src/app/simple-tween-scroll/simple-tween-scroll.component.ts
@@ -19,24 +19,24 @@ export class SimpleTweenScrollComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     // 创建控制器
-    let controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller();
 
     // 创建场景
-    let orangeScene = new ScrollMagic.Scene({
+    const orangeScene = new ScrollMagic.Scene({
       triggerElement: this.trigger1.nativeElement
     })
     .setTween( TweenMax.to('.orange', 0.5, {backgroundColor: 'green', scale: 2.5})) /*requeire import plugin 4k*/
     .addTo(controller);
 
     // build scene multiple
-    let pinkScene1 = new ScrollMagic.Scene({
+    const pinkScene1 = new ScrollMagic.Scene({
       triggerElement: this.trigger2.nativeElement,
       triggerHook: 0.8  /*0-1触发scene的位置*/
     })
     .setTween('.pink', 0.5, {x: 200})
     .addTo(controller);
 
-    let pinkScene2 = new ScrollMagic.Scene({
+    const pinkScene2 = new ScrollMagic.Scene({
       triggerElement: this.trigger2.nativeElement
     })
     .setTween('.red', 0.5, {x: 200, delay: 0.5})
