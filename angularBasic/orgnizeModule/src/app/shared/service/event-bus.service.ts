0dfa1e8a7116cb417ee43db32a59c049857e11a9
tinalim <lst_tina@163.com>
2019-01-30 10:17:30 +0800

    新增事件总线 | 国家化集成

---
+++ angularBasic/orgnizeModule/src/app/shared/service/event-bus.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+
+/**
+ * 事件总线
+ * 功能：全局状态管理
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class EventBusService {
+  // public test1: Subject<any> = new Subject<any>();
+}
